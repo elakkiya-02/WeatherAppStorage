dde7b5666bb96c3056746124890cd99b74222351
Elakkiya Rajendran <89492090+elakkiya-02@users.noreply.github.com>
2025-11-24 00:01:00 +0530

    deployment object creation
    updated with path and entrypoint as prefect adds the local working directory to the pull steps, overriding github storage config

--- src/data_ingestion/deploy_weather_flow.py
+++ src/data_ingestion/deploy_weather_flow.py
@@ -4,21 +4,26 @@ Created on Sat Nov 15 20:48:29 2025
 
 @author: elakkiya
 """
-import os
-import sys
-
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(ROOT_DIR, "src", "data_ingestion"))
 #%%
 from prefect.deployments import Deployment
 from fetch_open_meteo import fetch_open_meteo_data_flow
+from prefect_github.repository import GitHubRepository
 #%%
+
+#loading our github storage block
+
+github_repository_block = GitHubRepository.load("weather-storage")
 #creating our deployment object
-deployment = Deployment(name="open-meteo-deployment",
-                        flow = fetch_open_meteo_data_flow,
+deployment = Deployment(flow = fetch_open_meteo_data_flow,
+                        name="open-meteo-serverless",
+                        storage=github_repository_block,
+                        work_pool_name = "default", #serverless
+                        tags = ["weather", "serverless"],
+                        path = "src/data_ingestion",
+                        entrypoint="fetch_open_meteo.py:fetch_open_meteo_data_flow",
                         )
 #registering the deployment with Prefect Cloud
 if __name__ == "__main__":
     deployment.apply()
-    print("[INFO] Deployment created sucessfully...")
+    print("[INFO] serverless deployment created...")
 #%%
